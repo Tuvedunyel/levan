372a4a86a8f227443138a0b4d65458b4ae4b5b78
Grégoire <72552360+Tuvedunyel@users.noreply.github.com>
2022-04-29 09:57:09 +0200

    first commit

---
+++ functions.php
@@ -0,0 +1,28 @@
+<?php 
+
+  add_theme_support('post-thumbnails');
+  add_theme_support('title-tag');
+
+  function btg_register_assets() {
+    wp_enqueue_style('btg-style', get_template_directory_uri().'/css/style.css', 1.0);
+    wp_enqueue_scripts('btg-script', get_template_directory_uri().'/js/main.js', array(), '1.0', true);
+  }
+
+  add_action('wp_enqueue_scripts', 'btg_register_assets');
+
+
+
+  // Page d'options
+  if (function_exists('acf_add_options_page')) {
+    
+    acf_add_options_page(array(
+      'page_title'	=> 'Options du thème',
+      'menu_title'	=> 'Options',
+      'menu_slug'	=> 'options',
+      'capability'	=> 'edit_posts',
+      'redirect'	=> false,
+      'potision'	=> 2
+    ));
+  };
+
+  add_filter('wpcf7_autop_or_not', '__return_false');
