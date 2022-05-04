b100c98a5f97570d266b230164d58e36c50b11d6
Grégoire <72552360+Tuvedunyel@users.noreply.github.com>
2022-05-04 11:56:03 +0200

    catalogue

--- template-box.php
+++ template-box.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /*
 Template Name: Box du van
 */
@@ -15,5 +15,29 @@ get_header();
 			</div>
 		</div>
 	</article>
+	<?php if (have_rows('selection_boissons')) : ?>
+		<section class="selection" title='Notre sélection'>
+			<div class="container">
+				<ul class="selection-boissons">
+					<?php while (have_rows('selection_boissons')) : the_row(); ?>
+						<li>
+							<?php $image = get_sub_field('image'); ?>
+							<img src="<?= esc_url($image['url']); ?>" alt="<?= esc_attr($image['alt']); ?>">
+							<div class="selection-boissons__descriptif">
+								<h3><?php the_sub_field('titre'); ?></h3>
+								<?php the_sub_field('texte'); ?>
+							</div>
+						</li>
+					<?php endwhile; ?>
+				</ul>
+				<?php $lien = get_field('lien');
+				$target = $lien['target'] ? $lien['target'] : '_self';
+				?>
+				<a href="<?= esc_url($lien['url']); ?>" target="<?= esc_attr($target) ?>">
+					<?= esc_html($lien['title']); ?>
+				</a>
+			</div>
+		</section>
+	<?php endif; ?>
 </main>
 <?php get_footer(); ?>
