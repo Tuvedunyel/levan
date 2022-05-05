986a9232f1911552466fcf0963b47b83b08831b3
Grégoire <72552360+Tuvedunyel@users.noreply.github.com>
2022-05-05 14:42:10 +0200

    template particulier

---
+++ layouts/part-home.php
@@ -0,0 +1,7 @@
+
+<main class='home-template'>
+	<section class='home'>
+		<div class='red-bg'>
+			<h1><?php the_title(); ?></h1>
+			<article><?php the_content(); ?></article>
+		</div>
--- template-pro.php
+++ template-pro.php
@@ -2,21 +2,36 @@
 
 Template Name: Page Pro
 
- */ 
+ */
 get_header();
 ?>
 
-<main class='home-template'>
-	<section class='home'>
-		<div class='red-bg'>
-			<h1><?php the_title(); ?></h1>
-			<article><?php the_content(); ?></article>
-		</div>
+	<?php get_template_part('layouts/part-home'); ?>
 		<div class='right-side'>
 			<div class='hero-banner'>
 				<?php $image = get_field('image_haut_page'); ?>
 				<img src='<?= esc_url($image['url']) ?>' alt='<?= esc_attr($image['alt']) ?>' />
 			</div>
+			<?php $args = array(
+				'post_type' => 'post',
+				'posts_per_page' => 3
+			);
+			$post_query = new WP_Query($args);
+
+			if ($post_query->have_posts()) :
+			?>
+				<div class="post-loop">
+
+					<?php while ($post_query->have_posts()) :
+						$post_query->the_post();
+					?>
+						<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+					<?php
+					endwhile;
+					wp_reset_postdata();
+					?>
+				</div>
+			<?php endif; ?>
 		</div>
 	</section>
 </main>
